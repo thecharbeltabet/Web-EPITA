b7c5ad3753f558dd2f6429928906783129407bab
Charbel Tabet <charbeltabet33@hotmail.com>
2022-10-21 15:27:15 +0200

    Friday-21st-oct Commit 1

---
+++ Demo/main.js
@@ -0,0 +1,25 @@
+// console.log("hello world!");
+
+// let name = "Charbel"
+// let elie = 16
+//  let ne = name + elie;
+
+
+//  console.log(ne)
+
+//  let prioritiesn= ['error','warning','debug'];
+
+//  prioritiesn.forEach(let priority in prioritiesn)
+//  {
+//     console.log(priority);
+//  }
+
+// let titleElement = document.getElementById('title');
+
+
+// titleElement.addEventListener("click", (e) => {
+//     console.log("clicked!")
+// })
+
+
+
