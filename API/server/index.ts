1f62a01c94e293a7b1f939fd2b1ba413c729b46a
Charbel Tabet <charbeltabet33@hotmail.com>
2022-10-28 15:12:40 +0200

    Friday28Commit1

--- API/server/index.ts
+++ API/server/index.ts
@@ -1,26 +1,23 @@
 require('dotenv').config();
 // const express = require('express');
-import express from 'express'
-const morgan = require('morgan');
-const helmet = require('helmet');
+import express, {Request, Response} from 'express'
 import authRouter from './routes/auth'
 
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(helmet());
-app.use(express.json)
+app.use(express.json());
 
-app.get('/', (request, response) => {
-    response.status(200).send("Hello Charbel!");
-
+app.get('/', (request: Request, response: Response): void => {
+    response.status(200).send('Hello Loïc !');
 });
-
-
 
 app.use('/auth', authRouter);
 
 app.listen(process.env.APP_PORT, () => {
-    console.log(`Server runnning on http://localhost:${process.env.APP_PORT}` )
-})
+    console.log(`Server Running on http://localhost:${process.env.APP_PORT}`);
+});
--- API/server/routes/auth.ts
+++ API/server/routes/auth.ts
@@ -1,11 +1,12 @@
-import express, {Request, response, Response} from 'express';
+import express, {Request, Response} from 'express';
 
 let Router = express.Router();
 
-Router.post('/login', (request: Request, response: Response) => {
-console.log(request.body);
+Router.post('/login', (request: Request, response: Response): void => {
+    console.log(request.body);
 
-     response.status(200).send('Login Page')
+    
+    response.status(200).json('Login page');
 });
 
 export default Router;
