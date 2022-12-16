b184b9aff4eabb87622c21a699f9588db9841070
Charbel Tabet <charbeltabet33@hotmail.com>
2022-12-16 14:49:28 +0100

    Fri16Dec1

---
+++ API/front-test/src/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { App , Animals} from './app'
+import Default from './Layout/Default'
+
+function Router() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Default children={<App name="Tim" age={70}></App>}></Default>}></Route>
+        <Route path="/Animals" element={<Default children={<Animals></Animals>}></Default>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
